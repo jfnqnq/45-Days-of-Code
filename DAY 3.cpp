9e2563800143d1e99ac8e436881a9257ccb3c24b
Anusha Upadhyay <134798865+jfnqnq@users.noreply.github.com>
2023-09-21 02:05:54 +0530

    Add files via upload

---
+++ DAY 3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+     //your code goes here
+     int a,b,c;
+     for(int i=0;i<0;i++)
+     {
+       cin>>b>>c;
+       if(b>c)
+       {
+         cout<<b-c<<endl;
+       }
+       if(b<c)
+       {
+         cout<<"0"<<endl;
+       }
+     }
+     return 0;
+ }
+
