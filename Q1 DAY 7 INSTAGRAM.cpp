c8884b50771873901a152286b319d3f0a8535cae
Anusha Upadhyay <134798865+jfnqnq@users.noreply.github.com>
2023-10-15 13:52:34 +0530

    Add files via upload

---
+++ Q1 DAY 7 INSTAGRAM.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+        if (x > y * 10)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+    return 0;
+}
+
